7ea37d897d1819ddbe428e765f12ab9867e468e1
pablomoga9 <moyagarcia99@gmail.com>
2022-08-29 11:13:21 +0200

    add/auth

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("form2").addEventListener('submit', function(event){
+    event.preventDefault();
+    let email = document.getElementById("form2").email2.value;
+    let password = document.getElementById("form2").pass3.value;
+    if(email!= null||password!=null){
+        alert("No hay usuario o contraseña");
+    }
+    else if(password.length<6){
+        alert("La contraseña es demasiado corta");
+    }
+    else{
+        alert(`Bienvenido ${email}`);
+    }
+})
